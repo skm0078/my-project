76bf837728f4489eed945be454b59e27a571fd71
skm0078 <skmukherjee1994@gmail.com>
2023-02-13 23:46:34 +0530

    All dummy Ui added

---
+++ components/ContactMe.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { PhoneIcon } from "@heroicons/react/24/solid";
+
+type Props = {};
+
+const ContactMe = (props: Props) => {
+  return (
+    <div className="h-screen flex relative fex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+      <h3 className="absolute top-24 uppercase tarcking-[20px] text-gray-500 text-2xl">
+        Contact
+      </h3>
+
+      <div className="flex flex-col space-y-10">
+        <h4 className="text-4xl font-semibold text-center">
+          I have got what you need.{" "}
+          <span className="decoration-[#F7AB0A]/50 underline">Lets Talk</span>
+        </h4>
+
+        <div className="space-y-10">
+          <div className="flex items-center space-x-5 justify-center">
+            <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">+1122334455</p>
+          </div>
+          <div className="flex items-center space-x-5 justify-center">
+            <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">skmukherjee2021@gmail.com</p>
+          </div>
+          <div className="flex items-center space-x-5 justify-center">
+            <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">1123 Developer Lane</p>
+          </div>
+        </div>
+
+        <form className="flex flex-col space-y-2 w-fit mx-auto">
+          <div className="flex space-x-2">
+            <input type="text" placeholder="Name" className="contactInput" />
+            <input type="email" placeholder="Email" className="contactInput" />
+          </div>
+          <input type="text" placeholder="Subject" className="contactInput" />
+          <textarea className="contactInput" placeholder="Message" />
+          <button
+            type="submit"
+            className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ContactMe;
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,8 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import WorkExperience from "@/components/WorkExperience";
 import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
+import ContactMe from "@/components/ContactMe";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -45,8 +47,14 @@ export default function Home() {
       </section>
 
       {/* Projects */}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
 
       {/* Contact Me */}
+      <section id="contactme" className="snap-start">
+        <ContactMe />
+      </section>
     </div>
   );
 }
